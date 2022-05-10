2fe33f72ee431a2dd5840a053f1b78a0a6741452
Yavuz1997 <yavuzgogus@gmail.com>
2022-05-10 17:30:28 +0300

    Added jumping and left/right movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D playerRb;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float dirX = Input.GetAxisRaw("Horizontal");
+        playerRb.velocity = new Vector2(dirX * 10f, playerRb.velocity.y);
+        if(Input.GetButtonDown("Jump")){
+            playerRb.velocity = new Vector2(playerRb.velocity.x,15f);
+        }
     }
     
 }
