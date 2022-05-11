ea611719d73361240c529681c8da1f7976276142
Yavuz1997 <yavuzgogus@gmail.com>
2022-05-11 11:00:49 +0300

    Camera positioning update, jump and run animations, and script update for animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D playerRb;
+    private Animator playerAnim;
+    private SpriteRenderer playerSprite;
+    [SerializeField] private float runSpeed = 7f;
+    [SerializeField] private float jumpForce = 15f;
+    private float dirX = 0f;
     // Start is called before the first frame update
     private void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        playerRb.velocity = new Vector2(dirX * 10f, playerRb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        playerRb.velocity = new Vector2(dirX * runSpeed, playerRb.velocity.y);
         if(Input.GetButtonDown("Jump")){
-            playerRb.velocity = new Vector2(playerRb.velocity.x,15f);
+            playerRb.velocity = new Vector2(playerRb.velocity.x,jumpForce);
         }
+        animationStateUpdate();
     }
     
+    private void animationStateUpdate(){
+        if(dirX > 0f){
+            playerAnim.SetBool("Running", true);
+            playerSprite.flipX = false;
+        }
+        else if (dirX < 0f){
+            playerAnim.SetBool("Running", true);
+            playerSprite.flipX = true;
+        }
+        else{
+            playerAnim.SetBool("Running", false);
+        }
+    }
 }
